9a78eeb4dc2884b1c4b51e7a59df8be04d4b1bb0
ldev1996 <leonardo.dev1996@gmail.com>
2025-06-27 15:42:52 -0300

    refactor: changed structure to compatibility version 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
     compatibilityDate: '2025-05-15',
+    future: {
+        compatibilityVersion: 4,
+    },
     devtools: { enabled: true },
     pages: { pattern: ['**/*.vue', '!**/components/**'] },
     components: [
@@ -17,7 +20,6 @@ export default defineNuxtConfig({
     ],
     css: ['~/assets/css/main.css'],
     vite: { plugins: [tailwindcss()] },
-    srcDir: 'app',
     modules: ['@nuxtjs/color-mode', '@nuxt/icon'],
     colorMode: {
         preference: 'system', // default value of $colorMode.preference
