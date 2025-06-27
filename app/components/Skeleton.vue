1ae864fd7c48defa1ee73e94f84d0d6b029b331f
ldev1996 <leonardo.dev1996@gmail.com>
2025-06-27 16:47:40 -0300

    feat: added skeleton loader

---
+++ app/components/Skeleton.vue
@@ -0,0 +1,34 @@
+<template>
+    <div :class="['bg-surface animate-pulse shadow', r]" :style="size" />
+</template>
+
+<script setup>
+    const props = defineProps({
+        w: {
+            type: String,
+        },
+        h: {
+            type: String,
+        },
+        r: {
+            type: String,
+            default: '',
+            validator: (value) =>
+                [
+                    'rounded-sm',
+                    'rounded-md',
+                    'rounded-lg',
+                    'rounded-xl',
+                    'rounded-2xl',
+                    'rounded-3xl',
+                    'rounded-4xl',
+                    'rounded-full',
+                ].includes(value),
+        },
+    })
+
+    const size = computed(() => ({
+        width: props.w,
+        height: props.h,
+    }))
+</script>
