6a94457b5b0889f5ebc032c213c40307af86c353
ldev1996 <leonardo.dev1996@gmail.com>
2025-06-28 10:58:48 -0300

    feat, style: added header to default layout and adjusted index page styles

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,27 @@
 <template>
-    <main class="w-full h-dvh flex items-center justify-center bg-base">
+    <header class="px-4 md:px-8 py-3 md:py-6 w-full bg-base/90 shadow-lg z-10 fixed left-0 top-0 flex justify-between items-center">
+        <NuxtLink to="/"><Logo class="md:text-3xl"/></NuxtLink>
+        <nav>
+            <NuxtLink
+                v-for="link in navLinks"
+                :to="link.to"
+                :key="link.label"
+                class="text-foreground hover:text-primary transition-colors md:text-lg"
+            >
+                {{ link.label }}
+            </NuxtLink>
+        </nav>
+    </header>
+    <main>
         <slot />
     </main>
 </template>
+
+<script setup lang="js">
+    const navLinks = [
+        {
+            label: 'Como Funciona',
+            to: '#features'
+        }
+    ]
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,19 +1,19 @@
 <template>
     <section
-        class="bg-gradient-to-br from-primary from-15% to-secondary w-full min-h-dvh flex flex-col md:flex-row px-4 md:px-8 py-32"
+        class="bg-gradient-to-br from-primary from-15% to-secondary w-full h-dvh flex flex-col md:flex-row px-4 md:px-8 py-24 md:py-32"
     >
         <div class="flex flex-col md:w-1/2">
             <h1
-                class="text-foreground-invert text-4xl md:text-5xl lg:text-6xl font-bold"
+                class="text-foreground-invert text-2xl md:text-5xl lg:text-6xl font-bold"
             >
                 Conectando pessoas por um planeta mais sustentável
             </h1>
-            <p class="text-foreground-invert/90 text-xl mt-6">
+            <p class="text-foreground-invert/90 text-md md:text-xl mt-4 md:mt-6">
                 Troque, doe e receba itens recicláveis em sua comunidade.
                 Juntos, podemos reduzir o desperdício e construir um futuro mais
                 verde.
             </p>
-            <div class="mt-12 flex gap-6">
+            <div class="mt-8 md:mt-12 flex gap-6">
                 <NuxtLink class="btn-2" to="/auth/register">
                     Começar Agora
                 </NuxtLink>
@@ -21,11 +21,13 @@
             </div>
         </div>
     </section>
+    <section class="bg-base px-4 md:px-8">
+        <h1 id="features" class="text-3xl text-primary">Como Funciona</h1>
+    </section>
 </template>
 
 <script setup lang="js">
     definePageMeta({
-        layout: false,
         colorMode: 'light'
     })
 </script>
